3ca870b9d4ca5aae0e96a5c2485e2256b6de9f8c
Mark Hinschberger <markhinschberger@gmail.com>
2015-05-20 13:20:22 +0800

    Refactored calculator

--- teacalculator.rb
+++ teacalculator.rb
@@ -1,37 +1,28 @@
 # Mark Hinschberger
 #Github--> foodaka
 #email: markhinschberger@gmail.com
-require 'pry'
-def say(msg)
-	puts "=>  #{msg}"
+
+def say (msg)
+	puts "=> #{msg}"
 end
 
-puts "To exit the program press ctrl c"
 loop do
-puts "Enter a number"
-num1 = gets.chomp
+	say "Enter a number"
+	num1 = gets.chomp.to_f
+	say "the number is #{num1}"	
+	say "1) add 2) subtract 3) multiply 4)Divide"
+	operator = %i(+ - * /)[gets.chomp.to_i - 1]
 
-puts "=>the number is #{num1}"
+	if operator
+		say "operator is #{ operator }"
+	else
+		say "You did not enter 1-4."
+		break
+	end
+	
+	say "whats the second numer?"
+	num2 = gets.chomp.to_f
 
-puts "=>whats the second numer?"
-num2 = gets.chomp
-
-say "1) add 2) subtract 3) multiply 4)Divide"
-operator = gets.chomp
-
-if operator == '1'
-	result = num1.to_i + num2.to_i
-elsif operator == '2'
-	result = num1.to_i - num2.to_i
-elsif operator == '3'
-	result = num1.to_i * num2.to_i
-elsif operator == '4'
-	result = num1.to_f / num2.to_i
-elsif
-	puts "You did not enter 1-4"
-	break
+	result = num1.send(operator, num2)
+	say "#{ num1 } #{ operator } #{num2} = #{result}"
 end
-say "results is #{result}"
-end
-
-
